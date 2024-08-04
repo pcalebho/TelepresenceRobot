a292c38f27429dd9f1182e423a639ab7f79d568c
Caleb Ho <pcalebho@gmail.com>
2024-08-04 18:32:18 -0500

    added server.js from webrtc clone

--- web/server.js
+++ web/server.js
@@ -1,27 +1,41 @@
-const express = require('express');
-const ffmpeg = require('fluent-ffmpeg')
-const PORT = 3000;
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const cors = require('cors')
+const { v4: uuidV4 } = require('uuid')
+
+
+// // enabling access control origin
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 
 app.set('view engine', 'ejs')
+// app.use(cors());
+app.use(express.static('public'))
 
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('index', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
 
-        socket.on('disconnect', () => {
-        socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
 
-server.listen(PORT)
+server.listen(3000)
