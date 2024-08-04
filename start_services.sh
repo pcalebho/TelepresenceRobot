d116c104c0e4c51abdfcfab565e93337498208db
Caleb Ho <pcalebho@gmail.com>
2024-08-03 20:04:10 -0500

    add stopping script

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node web/server.js & ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+node web/server.js & echo $! > server.pid & ros2 launch rosbridge_server rosbridge_websocket_launch.xml
