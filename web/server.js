b2819aba368aff372c748fdd22de6cfa22a19bc9
Caleb Ho <pcalebho@gmail.com>
2024-08-03 19:37:40 -0500

    added video code with fluent mmpeg

--- web/server.js
+++ web/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const PORT = 3000;
+const ffmpeg = require('fluent-ffmpeg')
 // const socketIo = require('socket.io');
 // const SimplePeer = require('simple-peer');
 
@@ -7,7 +8,22 @@ const app = express();
 // const io = socketIo(server);
 
 app.use(express.static(__dirname + '/public'));
-// app.use('/scripts',express.static(__dirname+'./node_modules/'));
+
+app.get('/remoteVideo', (req, res) => {
+res.setHeader('Content-Type', 'video/mp4');
+    const ffmpegProcess = ffmpeg()
+        .input('/dev/video0') // Adjust the device path if necessary
+        .inputFormat('v4l2')
+        .videoCodec('libx264')
+        .format('mp4')
+        .on('end', () => {
+        console.log('Streaming finished.');
+    })
+    .on('error', (err) => {
+    console.error('An error occurred:', err);
+    })
+    .pipe(res, { end: true });
+});
 
 // io.on('connection', (socket) => {
 //   console.log('New client connected');
