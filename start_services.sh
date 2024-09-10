5aee65c9618e4fb9c2f1dcd9b711b3122120f818
Caleb Ho <pcalebho@gmail.com>
2024-09-09 22:20:02 -0500

    add node launch to bash script

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-node web/server.js & echo $! > server.pid & ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=9090
+node web/server.js & echo $! > server.pid & ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=9090 \
+    & source robot/install/setup.bash & ros2 run stepper stepper_motor_control
