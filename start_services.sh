7f0e44eaace9cc3471a0a290c90b59d777a2937f
Caleb Ho <pcalebho@gmail.com>
2024-09-14 00:47:41 -0500

    updated bash script for new folder name

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node web/server.js & echo $! > server.pid \
+node telebot_server/server.js & echo $! > server.pid \
  & source robot/install/setup.bash && ros2 launch stepper telebot_launch.py
